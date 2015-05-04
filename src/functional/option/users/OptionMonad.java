db8aa438f3fe5b497907096942d9960ec123967f
nizamk <nizam.ariffin@gmail.com>
2015-05-04 22:12:51 +0800

    update - map and flatmap

--- src/functional/option/users/OptionMonad.java
+++ src/functional/option/users/OptionMonad.java
@@ -16,7 +16,21 @@ public class OptionMonad extends AbstractDemo {
 	public void runFunctional() {
 		super.runFunctional();
 
-		Optional<User> user = UserRepository.findById(4);
-		user.ifPresent(t -> System.out.println(t.firstName + "," + t.lastName));
+		Optional<User> user = UserRepository.findById(3);
+
+		// Perform side-effect if value present
+		user.ifPresent(t -> System.out.println("Namd: " + t.firstName + "," + t.lastName));
+
+		// Mapping an option
+		Optional<Integer> age = UserRepository.findById(3).map(i -> i.age);
+		age.ifPresent(t -> System.out.println("Age: "+t));
+
+		// Nested monad
+		Optional<Optional<String>> gender = UserRepository.findById(3).map(i -> i.gender);
+
+		//Flatmapping an option
+		Optional<String> gender1 = UserRepository.findById(3).flatMap(i -> i.gender);
+		gender1.ifPresent(i -> System.out.println("Gender: " + i));
+
 	}
 }
